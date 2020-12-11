d146813587be705288f916030307c6efe58de48a
ZoltarSeven <50520285+ZoltarSeven@users.noreply.github.com>
2020-12-11 20:58:00 +0100

    Update Parcel.ts

--- src/Parcel.ts
+++ src/Parcel.ts
@@ -26,7 +26,7 @@ export function handleMintParcel(_: MintCall): void {
   parcel.length = _.inputs.x2 - _.inputs.x1
   parcel.width = _.inputs.z2 - _.inputs.z1
   parcel.area = parcel.length * parcel.width + _.inputs.y1
-  parcel.volumeInVoxels = parcel.area * parcel.buildHeight + 8
+  parcel.volumeInVoxels = parcel.area * parcel.buildHeight * 8
   parcel.owner = account.id
   parcel.createdAt = _.block.timestamp
   parcel.tokenURI = "https://www.cryptovoxels.com/p/" + parcel.tokenID.toString()
@@ -45,25 +45,30 @@ export function handleMintParcel(_: MintCall): void {
     else {
     widthLocation = parcel.width
     }
+    
   let west:  number = ((lengthLocation/2) - _.inputs.x2)
   let east:  number = ((lengthLocation/2) + _.inputs.x1)
   let north: number = (_.inputs.z1 + (widthLocation/2))
   let south: number = ((widthLocation/2) - _.inputs.z2)
-
-
+  
+  let westString = west.toString().split(".")[0]
+  let eastString = east.toString().split(".")[0]
+  let northString = north.toString().split(".")[0]
+  let southString = south.toString().split(".")[0]
+  
   if (_.inputs.x1 < 0 && _.inputs.z1 < 0) {
-    parcel.location = west.toString() + "W," + south.toString() + "S"
-    parcel.save()}
+  parcel.location = westString + "W," + southString + "S"
+  parcel.save()}
   else if (_.inputs.x1 < 0 && _.inputs.z1 > 0) {
-    parcel.location = west.toString() + "W," + north.toString() + "N"
-    parcel.save()}
+  parcel.location = westString + "W," + northString + "N"
+  parcel.save()}
   else if (_.inputs.x2 > 0 && _.inputs.z1 < 0) {
-    parcel.location = east.toString() + "E," + south.toString() + "S"
-    parcel.save()}
+  parcel.location = eastString + "E," + southString + "S"
+  parcel.save()}
   else  {
-    parcel.location = east.toString() + "E," + north.toString() + "N"
-    parcel.save()
-  }
+  parcel.location = eastString + "E," + northString + "N"
+  parcel.save()
+    }
 
 }
 
