91325c1f9cba60461e5a28d14a4c636802475db8
ZoltarSeven <50520285+ZoltarSeven@users.noreply.github.com>
2020-12-11 04:22:51 +0100

    Create Parcel.ts

---
+++ src/Parcel.ts
@@ -0,0 +1,85 @@
+import { Transfer, MintCall} from '../generated/Parcel/Parcel'
+import { Parcel, Account } from '../generated/schema'
+import { Address, log, store } from '@graphprotocol/graph-ts'
+
+
+function getOrCreateAccount(id: string) : Account {
+  let exists = Account.load(id)
+      if(!exists) {
+          let account = new Account(id)
+          account.address = Address.fromString(id)
+          account.save()
+          return account
+      } else return Account.load(id) as Account
+}
+
+export function handleMintParcel(_: MintCall): void {
+
+  let account = getOrCreateAccount(_.inputs._to.toHex())
+      account.address = _.inputs._to
+      account.save()
+
+  let parcel = new Parcel(_.inputs._tokenId.toHex())
+  parcel.tokenID = _.inputs._tokenId
+  parcel.buildHeight = _.inputs.y2 - _.inputs.y1
+  parcel.length = _.inputs.x2 - _.inputs.x1
+  parcel.width = _.inputs.z2 - _.inputs.z1
+  parcel.area = parcel.length * parcel.width + _.inputs.y1
+  parcel.volumeInVoxels = parcel.area * parcel.buildHeight + 8
+  parcel.owner = account.id
+  parcel.createdAt = _.block.timestamp
+  parcel.tokenURI = "https://www.cryptovoxels.com/p/" + parcel.tokenID.toString()
+  
+  
+
+  if (parcel.length % 2 == 1) {
+  parcel.length = parcel.length + 1
+  }
+  
+  let west:  number = ((parcel.length/2) - _.inputs.x2)
+  let east:  number = ((parcel.length/2) + _.inputs.x1)
+  let north: number = (_.inputs.z1 + (parcel.length/2))
+  let south: number = ((parcel.length/2) - _.inputs.z2)
+
+
+  if (_.inputs.x1 < 0 && _.inputs.z1 < 0) {
+    parcel.location = west.toString() + "W," + south.toString() + "S"
+    parcel.save()}
+  else if (_.inputs.x1 < 0 && _.inputs.z1 > 0) {
+    parcel.location = west.toString() + "W," + north.toString() + "N"
+    parcel.save()}
+  else if (_.inputs.x2 > 0 && _.inputs.z1 < 0) {
+    parcel.location = east.toString() + "E," + south.toString() + "S"
+    parcel.save()}
+  else  {
+    parcel.location = east.toString() + "E," + north.toString() + "N"
+    parcel.save()
+  }
+
+}
+
+export function handleTransferParcel(event: Transfer): void {
+
+if (event.params._to.toHex() == '0x0000000000000000000000000000000000000000'){
+    let id = event.params._to.toHex()
+    let account = getOrCreateAccount(id)
+    account.address = event.params._to
+    account.save()
+  
+    let parcel = Parcel.load(event.params._tokenId.toHex())
+    parcel.save()
+    store.remove('Parcel', event.params._tokenId.toHex())
+  }
+else if (event.params._from.toHex() != '0x0000000000000000000000000000000000000000'){
+  
+  let id = event.params._to.toHex()
+  let account = getOrCreateAccount(id)
+  account.address = event.params._to
+  account.save()
+
+  let parcel = Parcel.load(event.params._tokenId.toHex())
+  parcel.owner = account.id
+  parcel.save()
+  }
+
+}
